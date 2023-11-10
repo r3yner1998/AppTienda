b187a41d6e17fc7ae8ef7e2da9f12a9d830f53da
AxelGutierrezV <u21222595@utp.edu.pe>
2023-11-09 22:09:45 -0500

    Revert "Revert "provedores view fixed conection class fixed DAO added""
    This reverts commit 9e95271fc6e133f8ea745f6db929963ada2b586f.

---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,17 @@
+package Modelo;
+
+/**
+ *
+ * @author Axel
+ */
+public class Cliente extends Persona{
+
+    public Cliente() {
+    }
+
+    public Cliente(int codigo, String nombre, int documento, String correo, String telefono) {
+        super(codigo, nombre, documento, correo, telefono);
+    }
+
+
+}
